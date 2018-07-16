c593009b3d213ce0a4df8b4a592cd8a6afc30ac7
Ryan Becker <rbecker8@cnm.edugit config --global user.name Ryan>
2018-07-16 08:57:04 -0600

    Added notes above function in js file.

--- snap-sum.js
+++ snap-sum.js
@@ -1,3 +1,9 @@
+/**
+
+*@param {array} arrayToSum array of numerics to sum
+* @returns {number} value of sum
+
+ **/
 function sumArray(arrayToSum) {
 	let sum = 0;
 	for(let i = 0; i < arrayToSum.length; i++) {
